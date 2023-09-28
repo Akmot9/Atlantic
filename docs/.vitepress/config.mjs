07a524559b50c3c118ade7421df1ba69bae7c415
akmot9 <avicocyprien@yahoo.com>
2023-09-28 13:07:02 +0200

    config.msj added base repo

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "atlantic",
   description: "oui",
+  base: '/docs/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
